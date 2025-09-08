8705b1a00a43c6ea45b55791e449165cae4107a6
priceal <priceal@emmanuel.edu>
2025-09-08 14:45:49 -0400

    2025 09 08

--- untitled1.py
+++ untitled1.py
@@ -1,44 +1,6 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Apr 10 07:43:31 2025
+# Save model
+torch.save(model.state_dict(), "CNNonelength_20250908A_model.pth")
 
-@author: allen
-"""
-
-
-import os
-import numpy as np
-import matplotlib.pyplot as plt
-import torch
-
-from torch.utils.data import Dataset, DataLoader
-import CNN 
-
-class dataset(Dataset):
-    """  """
-
-    def __init__(self, x, y):
-        '''
-        
-        Parameters
-        ----------
-        x : TYPE
-            DESCRIPTION.
-        y : TYPE
-            DESCRIPTION.
-
-        Returns
-        -------
-        None.
-
-        '''
-        self.x = x
-        self.y = y
-
-    def __len__(self):
-        return len(self.x)
-
-    def __getitem__(self, idx):
-
-        return self.x[idx],self.y[idx]
+# Create new model and load states
+#newmodel = Multiclass()
+#newmodel.load_state_dict(torch.load("iris-model.pth"))
