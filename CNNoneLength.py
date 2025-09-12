50ba959f840ad56e23ec96f5b219c46fd70a6fd2
priceal <priceal@emmanuel.edu>
2025-09-12 12:42:30 -0400

    2025 10 12B

--- CNNoneLength.py
+++ CNNoneLength.py
@@ -16,10 +16,13 @@ uses class weights to correct imbalance in data
 features to include:
     1) employ torch optimization tools      v/
     2) use scikit learn metrics             v/
-    3) split test/train                     (pre-calculated)
+    3) split test/train                     v/ (pre-calculated)
     4) employ batch optimization            v/
     5) drop out back propagation
     6) use weights to correct imbalance     v/
+    7) add embedding                        v/
+    8) padding to handle variable length
+    9) add attention layer
     
 """
 
@@ -31,7 +34,7 @@ from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
 
 from ssp_utils import dataReader, seqDataset
-from model_20250912A import cnnModel
+from model_20250912B import cnnModel
 
 '''
 ###############################################################################
@@ -39,14 +42,13 @@ from model_20250912A import cnnModel
 #############################################################################################################################################################
 '''
 
-
 # learning parameters
-lengthLimits = (100,600)  # screen data for seq lengths in this interval
-cropSize = 100  # crop/pad all accepted seqs to this length
+lengthLimits = (400,600)  # screen data for seq lengths in this interval
+cropSize = 400  # crop/pad all accepted seqs to this length
 numBatches = 0  # if non-zero, ignore batchSize and set to N/numBatches
 batchSize = 10  # only use if numBatches = 0
-numberEpochs = 50
-reportCycle = 200
+numberEpochs = 20
+reportCycle = 100
 learningRate = 0.1
 weights = 'calc'    # None: unweighted. 
                     # (WH, WE, WC): use fixed weights
@@ -70,36 +72,36 @@ dataTrain = seqDataset(xTrain, yTrain) # needed for batches
 print("DATA SET ")
 rows = ['training data', 'training labels', 'test data', 'test labels']
 ds = [xTrain, yTrain, xTest, yTest]
-print("{:20} {:15} {:15} {:15}".format('DATA', 'ENTRIES', 'CHANNELS', 'LENGTH'))
+print("{:<20} {:<15} {:<15} {:<15}".format('DATA', 'ENTRIES', 'CHANNELS', 'LENGTH'))
 for r, d in zip(rows, ds):
     a, b, c = d.shape
     print(f"{r:<20} {a:<15} {b:<15} {c:<15}")
 if numBatches > 0:
     batchSize = int(len(xTrain)/numBatches)
 else:
-    numBatches = int(len(xTrain)/batchSize) + 1
-dataloader = DataLoader(dataTrain, batch_size=batchSize, shuffle=True)
-print('number of batches:', numBatches)
-print('size of batches:', batchSize)
-
-                
+    numBatches = int(len(xTrain)/batchSize)
+numClasses = yTrain.sum( dim=(0,2) ) 
+targetLabels = ['H', 'E', 'C']
 # create weights for classes--should broadcast correctly in loss calc
 if not weights:
     weights=torch.tensor((1.0,1.0,1.0))
 elif weights=='calc':
-    numClasses = yTrain.sum( dim=(0,2) )  # sums over entries and AA positions
+     # sums over entries and AA positions
     weights = numClasses.sum()/numClasses/3 # dims=(3)
 else:
     weights = torch.tensor(weights)
 weights.unsqueeze_(1)   # add dim in place to get dims = (3,1) for broadcasting
-print('')
-targetLabels = ['H', 'E', 'C']
-print('{:<10} {:<10} {:<10}'.format('','label','weight') )
+print('{:<10} {:<10} {:<10} {:<10} {:<10}'.format('index','label','count','fraction','weight') )
 for i,tl in enumerate(targetLabels):
-    print('{:<10} {:<10} {:<10}'.format( i, tl, float(weights[i,0]) ) )
+    print('{:<10} {:<10} {:<10} {:<10.4} {:<10.4}'.format(
+        i,tl,int(numClasses[i]),numClasses[i]/numClasses.sum(), float(weights[i,0]) 
+        ) )
+print('number of batches:', numBatches)
+print('size of batches:', batchSize)
+dataloader = DataLoader(dataTrain, batch_size=batchSize, shuffle=True)
 
 # create model
-#model = cnnModel()
+model = cnnModel()
 print('\nMODEL ')
 print("{0:20} {1:20}".format("MODULES", "PARAMETERS"))
 total_params = 0
@@ -115,7 +117,7 @@ print("{0:20} {1:<20}".format("TOTAL", total_params))
 plt.figure(1)
 optimizer = torch.optim.SGD(model.parameters(), lr=learningRate)
 print('\nOPTIMIZATION')
-print('{:10} {:10} {:10}'.format('epoch','batch','loss-train') )
+print('{:10} {:10} {:10} {:10}'.format('epoch','batch','loss-train','loss-test') )
 stepCount = 0
 for i in range(numberEpochs):
     for j, batch in enumerate(dataloader):
@@ -129,9 +131,16 @@ for i in range(numberEpochs):
         loss.backward()
         optimizer.step()
         
+        # print out if report cycle done
         if stepCount % reportCycle == 0:
-            print(f"{i:<10} {j:<10} {loss.item():<10.5}")
+            
+            # calc test loss
+            testPrediction = model( xTest ) 
+            testLossTerms = -yTest*torch.log(testPrediction)*weights
+            testLoss = testLossTerms.sum()/yTest.shape.numel() # normalize by num of AAs
+            print(f"{i:<10} {j:<10} {loss.item():<10.5} {testLoss.item():<10.5}")
             plt.plot([stepCount], [loss.item()], '.k')
+            plt.plot([stepCount], [testLoss.item()], '.r')
         
         stepCount += 1
 
@@ -157,26 +166,17 @@ print('{:10} {:10} {:10}'.format('class', 'recall', 'precision'))
 for n, r, p in zip(targetLabels, recall, precision):
     print(f'{n:<10} {r:<10.4} {p:<10.4}')
 
-labelCounts = cm.sum(axis=1)
-print('')
-print('{:<10} {:<10} {:<10} {:<10}'.format('label','count','fraction','weight') )
-for i,tl in enumerate(targetLabels):
-    print('{:<10} {:<10} {:<10.5} {:<10.5}'.format(
-        tl,labelCounts[i],labelCounts[i]/labelCounts.sum(), float(weights[i,0]) 
-        ) )
+print('\ntest set performance')
+yCheck = np.argmax(yTest.detach().numpy(), axis=1).flatten()
+prediction = model(xTest)
+pCheck = np.argmax(prediction.detach().numpy(), axis=1).flatten()
+cm = confusion_matrix(yCheck, pCheck)
+#disp = ConfusionMatrixDisplay(confusion_matrix=cm,
+#                              display_labels=['H', 'E', 'C'])
+#disp.plot()
+recall = np.diagonal(cm)/cm.sum(axis=1)
+precision = np.diagonal(cm)/cm.sum(axis=0)
+print('{:10} {:10} {:10}'.format('class', 'recall', 'precision'))
+for n, r, p in zip(['H', 'E', 'C'], recall, precision):
+    print(f'{n:10} {r:<10.4} {p:<10.4}')
 
-'''
-    print('\ntest set performance')
-    yCheck = np.argmax(yTest.detach().numpy(), axis=1).flatten()
-    prediction = model(xTest)
-    pCheck = np.argmax(prediction.detach().numpy(), axis=1).flatten()
-    cm = confusion_matrix(yCheck, pCheck)
-    disp = ConfusionMatrixDisplay(confusion_matrix=cm,
-                                  display_labels=['H', 'E', 'C'])
-    disp.plot()
-    recall = np.diagonal(cm)/cm.sum(axis=1)
-    precision = np.diagonal(cm)/cm.sum(axis=0)
-    print('{:10} {:10} {:10}'.format('class', 'recall', 'precision'))
-    for n, r, p in zip(['H', 'E', 'C'], recall, precision):
-        print(f'{n:10} {r:<10.4} {p:<10.4}')
-'''
---
+++ model_20250912B.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+deep narrow CNN for ssp.
+add embedding layer
+    
+"""
+
+import torch
+
+###############################################################################
+class cnnModel(torch.nn.Module):
+    '''
+    CNN that takes one-hot rep of sequence as input and outputs a one-hot
+    rep of the secondary structure prediction. each layer is a 1d conv.
+    followed by an activation.
+    '''
+
+    def __init__(self):
+        super(cnnModel, self).__init__()
+
+        # embedding transformation, input tensor must have dims (*,20), output
+        # will have (*,5). since CNN uses (*,channels,length), must swap last
+        # two dims before and after use
+        self.embedding = torch.nn.Linear( 20, 7, bias=False )
+        
+        # input channels = number of AA's +1 for padding. pad so that
+        # output is same length as input, padding character = 0
+        # layer parameters = (input chans, output chans, kernel)
+        self.layer1 = torch.nn.Conv1d(in_channels=7,
+                                      out_channels=5,
+                                      kernel_size=11,
+                                      stride=1,
+                                      padding='same')
+        self.relu1 = torch.nn.ReLU()
+
+        self.layer2 = torch.nn.Conv1d(in_channels=5,
+                                      out_channels=5,
+                                      kernel_size=11,
+                                      stride=1,
+                                      padding='same')
+        self.relu2 = torch.nn.ReLU()
+
+        self.layer3 = torch.nn.Conv1d(in_channels=5,
+                                      out_channels=5,
+                                      kernel_size=11,
+                                      stride=1,
+                                      padding='same')
+        self.relu3 = torch.nn.ReLU()
+
+        self.layer5 = torch.nn.Conv1d(in_channels=5,
+                                      out_channels=3,
+                                      kernel_size=11,
+                                      stride=1,
+                                      padding='same')
+        
+        self.softMax5 = torch.nn.Softmax(dim=1)
+
+    def forward(self, x):
+
+        # since CNN uses (*,channels,length), must swap last
+        # two dims before and after embedding multiplication
+        x = self.embedding(x.transpose(-1,-2))
+        x = torch.transpose(x,-1,-2)
+        
+        x = self.layer1(x)
+        x = self.relu1(x)
+               
+        x = self.layer2(x)
+        x = self.relu2(x)
+        
+        x = self.layer3(x)
+        x = self.relu3(x)
+
+        x = self.layer5(x)
+        x = self.softMax5(x)
+
+        return x
